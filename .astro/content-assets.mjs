efa18ceec6394df56eebfa7a1e2df049e19f0a45
Jin Woo-heon <188007917+NickTerty@users.noreply.github.com>
2025-07-21 22:26:24 +0800

    IDK

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,27 +1,8 @@
-import __ASTRO_IMAGE_IMPORT_Zoga71 from "./6TMT/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
+
 import __ASTRO_IMAGE_IMPORT_624U5 from "./6TMT/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
 import __ASTRO_IMAGE_IMPORT_AkjWb from "./6TMT/3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
 import __ASTRO_IMAGE_IMPORT_15CyYh from "./6TMT/4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
 import __ASTRO_IMAGE_IMPORT_1zUO1n from "./6TMT/5.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
-export default new Map([
-	[
-		"./6TMT/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md",
-		__ASTRO_IMAGE_IMPORT_624U5,
-	],
-	[
-		"./6TMT/3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md",
-		__ASTRO_IMAGE_IMPORT_AkjWb,
-	],
-	[
-		"./6TMT/4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md",
-		__ASTRO_IMAGE_IMPORT_15CyYh,
-	],
-	[
-		"./6TMT/5.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md",
-		__ASTRO_IMAGE_IMPORT_1zUO1n,
-	],
-	[
-		"./6TMT/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md",
-		__ASTRO_IMAGE_IMPORT_Zoga71,
-	],
-]);
+import __ASTRO_IMAGE_IMPORT_Zoga71 from "./6TMT/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md";
+export default new Map([["./6TMT/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md", __ASTRO_IMAGE_IMPORT_624U5], ["./6TMT/3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md", __ASTRO_IMAGE_IMPORT_AkjWb], ["./6TMT/4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md", __ASTRO_IMAGE_IMPORT_15CyYh], ["./6TMT/5.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md", __ASTRO_IMAGE_IMPORT_1zUO1n], ["./6TMT/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6tmt_feedback.md", __ASTRO_IMAGE_IMPORT_Zoga71]]);
+		
--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "NickTerty's Website",
-	subtitle: "",
+	subtitle: "A personal blog and portfolio.",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 85, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
+		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
