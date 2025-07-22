841493225bdac48e962ece013dbd57a839534475
Jin Woo-heon <188007917+NickTerty@users.noreply.github.com>
2025-07-22 12:50:54 +0800

    osu icon

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 85, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
@@ -59,7 +59,7 @@ export const profileConfig: ProfileConfig = {
 	links: [
 		{
 			name: "Twitter(X)",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			icon: "fa6-brands:x-twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://x.com/NickTerty",
@@ -76,7 +76,7 @@ export const profileConfig: ProfileConfig = {
 		},
 		{
 			name: "osu!",
-			icon: "fa6-regular:circle",
+			icon: "simple-icons:osu",
 			url: "https://osu.ppy.sh/users/17847990",
 		},
 	],
